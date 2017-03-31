114dd092954f3698acb47621ad5e2dd8bb942463
alexburzak <alexb@upmaster.co.il>
2017-03-31 14:41:54 +0300

    Added the bridge header of April

---
+++ iBeacon/AprilSDKHeader.h
@@ -0,0 +1,7 @@
+
+#ifndef AprilSDKHeader_h
+#define AprilSDKHeader_h
+
+#import <AprilSDK/AprilBeaconSDK.h>
+
+#endif /* AprilSDKHeader_h */
--- iBeacon/utils/Constants.swift
+++ iBeacon/utils/Constants.swift
@@ -3,4 +3,5 @@ import UIKit
 public struct Constants {
     
     static let GET_BEACON_URL = "http://185.53.170.86/api/ibeacon"
+    static let MINIMUM_BACKGROUND_FETCH_INTERVAL = 15.0
 }
