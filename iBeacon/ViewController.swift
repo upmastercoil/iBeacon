469bb14c6ea6f612b54af250a9aaa15934019265
alexburzak <alexb@upmaster.co.il>
2017-04-28 13:22:49 +0300

    Working with ABBluetoothManagerDelegate, ABBeaconManagerDelegate together.

--- iBeacon/ViewController.swift
+++ iBeacon/ViewController.swift
@@ -1,9 +1,11 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, ABBeaconManagerDelegate {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, ABBluetoothManagerDelegate, ABBeaconManagerDelegate {
     
+    var bluetoothBeacons = [ABBeacon]()
     var aprilBeacons = [ABBeacon]()
+    var bluetoothManager:ABBluetoothManager?
     var beaconManager:ABBeaconManager?
     var beaconRegion:ABBeaconRegion?
     
@@ -11,30 +13,50 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     override func viewDidLoad() {
         
-        func initUpBeacons() {
+        func initBeacons() {
             
             self.beaconManager = ABBeaconManager()
             self.beaconManager?.delegate = self as ABBeaconManagerDelegate
-            let beaconUUID = UUID()
-            beaconRegion = ABBeaconRegion(proximityUUID: beaconUUID, identifier: Constants.IBEACON_REGION)
-            self.beaconManager?.startRangingBeacons(in: beaconRegion)
+        }
+        
+        func initBluetooth(){
+            
+            self.bluetoothManager = ABBluetoothManager()
+            self.bluetoothManager?.delegate = self as ABBluetoothManagerDelegate
+            self.bluetoothManager?.startAprilBeaconsDiscovery()
         }
         
         super.viewDidLoad()
-        initUpBeacons()
+        
+        initBluetooth()
+        initBeacons()
+
         collectionViewBeacons.delegate = self
         collectionViewBeacons.dataSource = self
     }
     
     func beaconManager(_ manager: ABBeaconManager!, didRangeBeacons beacons: [Any]!, in region: ABBeaconRegion!) {
         
-        aprilBeacons.removeAll()
+        //aprilBeacons.removeAll()
+        print("")
+        print("---=================---")
+        
         for beacon in beacons{
-            aprilBeacons.append(beacon as! ABBeacon)
+            //aprilBeacons.append(beacon as! ABBeacon)
         }
         
         collectionViewBeacons.reloadData()
         //TODO Noy
+    }
+    
+    func beaconManager(_ manager: ABBluetoothManager!, didDiscover beacon: ABBeacon!) {
+        
+        if bluetoothBeacons.contains(beacon) != true{
+            bluetoothBeacons.append(beacon)
+        }
+        
+        collectionViewBeacons.reloadData()
+        
     }
     
     func numberOfSections(in collectionViewBeacons: UICollectionView) -> Int {
@@ -44,17 +66,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionViewBeacons: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return aprilBeacons.count
+        return bluetoothBeacons.count
     }
     
     func collectionView(_ collectionViewBeacons: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let beacon:ABBeacon = aprilBeacons[indexPath.item]
+        let beacon:ABBeacon = bluetoothBeacons[indexPath.item]
         
         let peripheral:CBPeripheral = beacon.peripheral
         let cell = collectionViewBeacons.dequeueReusableCell(withReuseIdentifier: "beaconCell", for: indexPath) as! UpBeaconCell
         let uuid = peripheral.identifier.uuidString
         cell.textViewBeacon.text = peripheral.name! + " - UUID = " + uuid
+        
+        beaconRegion = ABBeaconRegion(proximityUUID: peripheral.identifier, identifier: Constants.IBEACON_REGION)
+        self.beaconManager?.startRangingBeacons(in: beaconRegion)
+        
         return cell
     }
     
